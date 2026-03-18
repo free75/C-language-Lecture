bbfbcbb864e0b9cb35c4ceb88f0b9d138485c6a3
free75 <hyunho2st@gmail.com>
2026-03-18 19:47:21 +0900

    2026 3. 18

--- Program/Source.C
+++ Program/Source.C
@@ -1,126 +1,133 @@
 #include <stdio.h>
 
 
-//contrl A + control C = 복사 , control V = 붙여넣기
-//control S = 저장하기
 void main()
 {
-#pragma region 변수
-	//데이터를 저장할 수 있는 매모리 공간을 생성하는 것입니다. 여러 코드들을 묶어놓을 수 있는 기능이다.
+#pragma region 산술 연산자
+
+	//int HP = 1;
+
+	//int X = 10;
+	//int Y = 5;
+	//int G = 3;
+	//int B = 6;
+
+	//// 1. 변수 = 변수 + 변수
+
+	//HP = X + Y;
+
+	//printf("HP : %d\n", HP);
 
 
-#pragma endregion
+	//// 2, 변수 = 변수 - 리터럴 상수
 
-#pragma region 자료형
-	// 데이터를 저장하기 위해 데이터의 형태를 정해주는 것입니다. 
-	// 자료형은 각각의 자료형마다 크기가 정해져 있으며, 자료형의
-	// 크기는 바이트 단위로 이루어져 있으며, 자료형의 경우 자료형에
-	// 따라 저장할 수 있는 값의 종류와 범위가 결정됩니다.
+	//// 3. 변수 = 변수 + 심솔릭 상수
 
-#pragma endregion
-#pragma region 변수의 이름 규칙
+	//// 4. 변수 = 리터럴 상수 / 리터럴 상수
 
-	// 1. 변수의 이름은 대소문자를 구분합니다.
-	// ex) int health;
-	// ex) int HEALTH;
-
-	// 2. 변수의 이름으로 예약어를 사용할 수 있습니다.
-	// ex) char char;
-
-	// 3. 변수의 이름에는 공백이 포함될 수 없습니다.
-	// ex) float time Delta;
-
-	// 4. 변수의 이름으로 특수 문자는 "_", "$"만 사용할 수 있습니다.
-	// ex) long currency$;
-	// ex) short under_score;
-
-	// 5. 변수의 이름은 숫자로 시작할 수 없습니다.
-	// ex) int 5day
-	// ex) int year2022;
-
-	// 화면 확대 or 축소하는 방법 : control + 마우스 휠로 조정
-#pragma endregion
-
-#pragma region 서식 지정자
-	// 변수의 값을 출력하기 위해 자료형의 정보를 명시적으로
-	// 지정해주는 것입니다.
-
-
-
-
-	// * 중요 원하는 부분만 디버거 하는 방법: 왼쪽을 마우스 커서로 움직일 경우 동그라미들이 보이게 된다.
-	// 그걸 클릭할 경우, 디버거할 부분의 처음과 끝을 지정할 수 있게 되는데
-	// 하나씩 확인하려면, 맨 아래에 로컬이라는 글자를 클릭 후 지정한 디버거 구간 안에 있는
-	// 문장들을 창 위에 있는 아래 화살표 모양인 한 단계씩 코드 실행을 클릭 후 디버거를 할 수 있다.
-
-
-	char character = 'A';
-
-	int integer = 10;
-
-	float decimal = 5.5f;
-
-	int HP = 300;
-
-	// 서식 지정자의 경우 하나의 표준 출력 함수에 여러 개의
-	// 서식을 넣을 수 있으며, 서식 지정자의 순서는 왼쪽에서
-	// 부터 시작합니다.
-
-	printf("character : %c \ninteger : %d \ndecimal : %f", character, integer, decimal);
-	// 서식 지정자는 각각의 서식에 따라 원하는 데이터를 출력
-	//할 수 있으며, 서식과 변수의 형태가 일치하지 않으면
-	//원하는 값이 출력되지 않습니다.
-
-	// 값을 바꿀때엔 자료형 char, int, float을 다시 한번 쓸 필요가 없다.
-
-	character = 'B';
-
-	integer = 25;
-
-	decimal = 7.25f;
-
-	printf("\n");
-
-	// 변수는 프로그램이 실행되는 동안 값을 바꿀 수 있으며,
-	// 원래 저장되어 있는 값의 새로 저장되는 값에 의해 지워집니다.
-
-	printf("charater : %c\n : ", character);
-	printf("integer  : %d\n : ", integer);
-	printf("decimal : %f\n : ", decimal);
-                
-	
-#pragma endregion 
-
-
-
-
-#pragma region 상수
-	// 프로그램이 실행되는 동안 더 이상 값을 변경할
-	// 수 없는 메모리 공간입니다.
-
-	const float pi = 3.141592f;
-
-	// 상수의 경우 메모리 공간을 가지고 있지 않은 상수를
-	// 리터럴 상수라고 하며, 메모리 공간을 가지고 있는
-	// 상수를 심볼릭 상수라고 합니다.
-
-	// ex) pi = 6.125f;
-
-	printf("상수의 값 : %f\n", pi);
-
-	//상수는 메모리 공간을 생성하는 동시에 초기화해야 하며,
-	// 한 번 저장된 값은 더 이상 변경할 수 없습니다.
-
-#pragma endregion
-
-
-#pragma region
-
-
+	//// 5. 변수 = 심볼릭 상수 % 리터럴 상수
 
 
 
 #pragma endregion
+
+#pragma region 비트
+	// 데이터를 나타내는 최소의 단위이며, 0 또는 1의
+	// 조합으로 논리 계산을 수행하는 단위입니다.
+
+	//pragma region의 뒤에 숫자를 쓰려면 ()로 감싸야 오류가 안 생깁니다.
+
+#pragma region (10)진수를 (2)진수로 변환하는 과정
+	// 10진수를 1이 될 때까지 계속 2로 나누어 준 다음
+	//나눈 위치의 나머지 값을 아래에서 위로 순서대로 정렬합니다.
+
+	// ex) 23 = 10111(2) 
+
+
+#pragma endregion
+#pragma region (2)진수를 (10)진수로 변환하는 과정
+	// 1 byte에 2진수로 저장된 값을 2의 제곱으로 나타냅니다.
+	//각각의 비트에 1이 있다면 1과 2의 제곱의 위치를 계산한
+	//다음 각각의 비트를 모두 더하여 10진수로 나타냅니다.
+
+
+#pragma endregion
+#pragma region 비트 연산자
+	// 비트 단위로 논리 연산을 수행하기 위해 사용하는 단위입니다.
+
+#pragma region AND 연산자
+	// 두 개의 피연산자가 모두 1이면 1을 반환하는 연산자입니다.
+	// AND 연산자는 &를 사용해야한다.
+
+	int wisdom = 10; // 0000 1010
+	int dexterity = 6; // 0000 0110
+
+
+	printf("wisdom & dexterity : %d\n", wisdom & dexterity);
+
+
+#pragma region or 연산자
+	// or 연산자의 기호는 흔히 쓰는 줄바꿈의 기호를 시프트와 같이 쓰면 나오는 기호이다.
+
+	int poison = 8;  // 0000 1000
+	int freeze = 12; // 0000 1100
+	// ------------
+	// 0000 1100
+	printf("poison & freeze : %d\n", poison | freeze);
+#pragma endregion
+
+#pragma region XOR 연산자
+	// 두 개의 피연산자가 서로 같으면 0을 반환하고, 서로 다르면
+	// 1을 반환하는 연산자입니다.
+	// xor 연산자의 기호는 숫자 6을 시프트 하고 나온 기호이다.
+
+	int idle = 15;  // 0000 1111
+	int battle = 6; // 0000 0110
+	                // ----------
+	                // 0000 1001
+
+	printf("idle ^ battle : %d\n", idle ^ battle);
+
+
+#pragma endregion
+#pragma region NOT 연산자
+	// 하나의 비트를 반전시키는 연산자입니다.
+	// NOT 연산자의 기호는 물결무늬인 ~ 이다. `를 시프트와 동시에 누르면 나오는 기호다.
+	int status = 13; // 0000 1101
+	                 // ---------
+	                 // 1111 0010
+	// NOT 연산자의 기호는 정한 변수의 이름 바로 앞에 (예시 : ~변수) 사용해야 실행할 때 제대로 된 값이 나온다.)
+	printf("~status : %d\n", ~status);
+
+	// 첫 번째 비트는 부호를 나타내며, 첫 번째 비트에
+	// 1이 있다면 값은 음수가 됩니다.
+	// 즉 13은 NOT 연산자를 쓰게 되면 첫 번째 비트에 1이 있으니 음수로 변하게 됩니다.
+
+
+#pragma endregion
+
+#pragma region 시프트 연산자
+	//  비트를 특정한 위치로 정해진 수만큼 이동시키는 연산자입니다.
+	// 시프트 연산자의 기호는 <<이다.
+
+	int packet = 5; // 0000 0101
+	                // 왼쪽 2칸 이동
+	                // 0000 1010
+	                // 0001 0100
+
+	printf("packet 변수를 왼쪽으로 2칸 이동한 결과 : %d\n", packet << 2);
+
+	printf("packet 변수를 오른쪽으로 1칸 이동한 결과 : %d\n", packet >> 1);
+
+
+
+#pragma endregion
+
+
+#pragma endregion
+
+#pragma endregion
+
 
 
 }
